f72b2000e6d162969587a6f4ed236e7e3bad92ca
Cintiaa <cintiasantos@alunos.utfpr.edu.br>
2019-07-30 23:15:23 -0300

    ajustes layout

---
+++ frontend/src/app/professor/professor.component.ts
@@ -0,0 +1,12 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, Component } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+
+@Component({
+    templateUrl: 'professor.component.html',
+    styleUrls: ['professor.component.css']
+  })
+export class ProfessorComponent { 
+    constructor(){}
+}
+
